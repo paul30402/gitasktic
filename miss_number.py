50256d73a9a6df131896eb49a9fb1dadb2e01977
MKL <kanboy18@hotmail.com>
2021-05-14 18:41:42 +0800

    Version 01: miss_number.py; a tiny revise

--- miss_number.py
+++ miss_number.py
@@ -1,7 +1,7 @@
 while True:
     try:
-        iter_str = input('Take in numbers: ')
-        iter_str = iter_str.split()
+        iter_str = input('Take in numbers, e.g. 1, 2, 3, 4, 5: ')
+        iter_str = iter_str.split(', ')
         iter_int = []
         for i in iter_str:
             iter_int.append(int(i))
